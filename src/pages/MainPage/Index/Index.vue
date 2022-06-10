2dda55885823b755448a8d22e5fbf5cf37bf66dd
Azur冰弦 <973562770@qq.com>
2022-06-10 11:30:52 +0800

    Update: Index content

--- src/pages/MainPage/Index/Index.vue
+++ src/pages/MainPage/Index/Index.vue
@@ -1,11 +1,22 @@
 <template>
-  <PlayerList></PlayerList>
+  <div class="flex-1 bg-white rounded-lg ml-4 mt-4 mr-4 p-4 shadow">
+    <article class="prose ">
+      <h1>AzurCraftHelper</h1>
+      <p>咕咕咕咕咕咕在写了在写了</p>
+      <p>Waibiwaibi</p>
+    </article>
+  </div>
+  <div class="flex">
+    <PlayerList></PlayerList>
+    <ServerList></ServerList>
+  </div>
 </template>
 
 <script>
 import PlayerList from '../../../components/PlayerList.vue'
+import ServerList from '../../../components/ServerList.vue'
 export default {
-  components: {PlayerList}
+  components: { PlayerList, ServerList }
 }
 </script>
 
