8179784877dd43bce2e95ae79013efec6a54905b
Azur冰弦 <973562770@qq.com>
2023-05-20 18:44:39 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
 import App from './App.vue'
 import './index.css'
 import 'remixicon/fonts/remixicon.css'
 import router from './router'
 import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+co// nst pinia = createPinia()
+
+createApp(App)
+    .use(router)
+    .use(store)
+    // .use(pinia)
+    .mount('#app')
+
